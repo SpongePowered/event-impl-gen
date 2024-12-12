ef4266c2df053e396e8cf2b5228e453ca97d3f87
Gabriel Harris-Rouquette <github@gabizou.com>
2024-12-11 20:30:58 -0800

    Add Gson dependency and module requirement
    * Add `requires com.google.gson` to `module-info.java`
    * Add Gson library entry to `libs.versions.toml`
    * Add Gson dependency to `dependencies` section in `build.gradle`

--- annotations/src/main/java/module-info.java
+++ annotations/src/main/java/module-info.java
@@ -4,4 +4,6 @@ module org.spongepowered.eventimplgen.annotations {
     // This way we can still use the internal package, but not
     // to external developers.
     exports org.spongepowered.eventgen.annotations.internal to org.spongepowered.eventimplgen;
+
+    requires com.google.gson;
 }
